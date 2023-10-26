f19dd8f112d9fc23281c0d1c08f17bfadb8b14ae
abhaybissa31 <theabhaybissa@gmail.com>
2023-10-27 00:21:35 +0530

    testing details page

--- controllers/Details.js
+++ controllers/Details.js
@@ -5,29 +5,32 @@ res.render("details");
 }
 const addDetails=(userData)=>async(req,res)=>{
     var response=[];
-    let result= await userData.updateOne({ _id: req.session.user._id }, { $set: { u_city:req.body.u_city,u_phone:req.body.u_phone} })
-       if(result){
-        response.push(
-            {
-            "success":'true',
-           },
-           {
-
-               "_id":req.session.user._id
-           },
-           {
-            "msg":"Details Have Been Updated "
-           }
-        )
-       }else{
-        response.push({
-            "success":'false'
-        },
-           {
-            "msg":"ERR in Database"
-           }
-        )
-       }
+    let result= await userData.updateOne({ _id: req.session.user._id }, { $set: { u_city:req.body.u_city,u_phone:req.body.u_phone} },(err,result)=>{
+        if(err){
+            response.push({
+                "success":'false'
+            },
+               {
+                "msg":"ERR in Database"
+               }
+            )
+        }
+        else{
+            response.push(
+                {
+                "success":'true',
+               },
+               {
+    
+                   "_id":req.session.user._id
+               },
+               {
+                "msg":"Details Have Been Updated "
+               }
+            )
+        }
+    })
+      
 res.send(response)
 
 };
