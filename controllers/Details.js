2fe9b3967611c42981eca2c806530e4a14203179
abhaybissa31 <theabhaybissa@gmail.com>
2023-10-27 11:27:38 +0530

    update 3

--- controllers/Details.js
+++ controllers/Details.js
@@ -1,37 +1,32 @@
 const getDetailsFile=(req,res)=>{
 // res.send("details");
-console.log("hey");
 res.render("details");
 }
 const addDetails=(userData)=>async(req,res)=>{
     var response=[];
-     await userData.updateOne({ _id: req.session.user._id }, { $set: { u_city:req.body.u_city,u_phone:req.body.u_phone} },(err,result)=>{
-        if(err){
-            response.push({
-                "success":'false'
-            },
-               {
-                "msg":"ERR in Database"
-               }
-            )
-        }
-        else{
-            console.log(req.session)
-            response.push(
-                {
-                "success":'true',
-               },
-               {
-    
-                   "data":result
-               },
-               {
-                "msg":"Details Have Been Updated "
-               }
-            )
-        }
-    })
-      
+    let result= await userData.updateOne({ _id: req.session.user._id }, { $set: { u_city:req.body.u_city,u_phone:req.body.u_phone} })
+       if(result){
+        response.push(
+            {
+            "success":'true',
+           },
+           {
+
+               "_id":req.session.user._id
+           },
+           {
+            "msg":"Details Have Been Updated "
+           }
+        )
+       }else{
+        response.push({
+            "success":'false'
+        },
+           {
+            "msg":"ERR in Database"
+           }
+        )
+       }
 res.send(response)
 
 };
